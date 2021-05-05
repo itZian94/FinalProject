6d7394b0efe2906501cf06a72efb793f2a7812f3
Ubuntu <ubuntu@ip-172-31-12-74.eu-west-2.compute.internal>
2021-05-05 11:51:54 +0000

    config update

--- jenkins/configuration.sh
+++ jenkins/configuration.sh
@@ -1,16 +1,16 @@
 echo "Managing Terraform configuration"
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y unzip wget
-wget https://releases.hashicorp.com/terraform/0.14.6/terraform_0.14.6_linux_amd64.zip
-unzip terraform_*_linux_*.zip
+sudo wget https://releases.hashicorp.com/terraform/0.14.6/terraform_0.14.6_linux_amd64.zip
+sudo unzip terraform_*_linux_*.zip
 sudo mv terraform /usr/local/bin
-rm terraform_*_linux_*.zip
+sudo rm terraform_*_linux_*.zip
 
 echo "Preparing eksctl for Amazon cluster"
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz"| tar xz -C /tmp
+sudo curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz"| tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 
 echo "Setting up cluster API through kubectl"
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl
-chmod +x ./kubectl
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl 
