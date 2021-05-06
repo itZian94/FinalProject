60c7672fdfef669e6399d756443f963d04b2f88b
ddiazsouto <ddiazsouto@gmail.com>
2021-05-06 09:19:05 +0100

    Testing deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,7 +1,7 @@
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws configure set region eu-west-2
-aws configure set output json
+# aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+# aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+# aws configure set region eu-west-2
+# aws configure set output json
 
 aws eks --region eu-west-2 update-kubeconfig --name final_project_cluster
 
