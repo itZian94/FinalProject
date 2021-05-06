d87142c421685597cc675416bf06578528659063
ddiazsouto <ddiazsouto@gmail.com>
2021-05-06 09:34:14 +0100

    outputting ip

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,7 +1,3 @@
-# aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-# aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-# aws configure set region eu-west-2
-# aws configure set output json
 
 aws eks --region eu-west-2 update-kubeconfig --name final_project_cluster
 
@@ -12,8 +8,4 @@ kubectl apply -f nginxService.yaml
 kubectl apply -f ./nginx/configmap.yaml
 kubectl apply -f ./nginx/nginxDeployment.yaml
 
-# kubectl get pod -o wide
-# kubectl get events
-# kubectl get deployment
-# kubectl get service
-# kubectl get svc nginx-service
+kubectl get svc | grep balanceload
